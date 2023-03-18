09c66705e39d8478be0cedae0637318c6698dbb6
DESKTOP-E8F9381\admin <bestriven.weasky@gmail.com>
2023-03-18 21:41:05 +0700

    up

--- src/components/ShareScreen/index.tsx
+++ src/components/ShareScreen/index.tsx
@@ -1,41 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { createScreenVideoTrack } from "agora-rtc-react";
 
 const useScreenVideoTrack = createScreenVideoTrack({
-      encoderConfig: "720p_3",
-    });
+  encoderConfig: "720p_3",
+});
 
-export default function ShareScreen({ client, setShareScreen, tracks, hasCam }: any) {
+export default function ShareScreen({
+  client,
+  setShareScreen,
+  tracks,
+  hasCam,
+}: any) {
   const screenTracks = useScreenVideoTrack().tracks;
 
   const [trackCam, setTrackCam] = useState(tracks);
 
   const startShareScreen = async () => {
     if (hasCam) {
-      await client.unpublish(tracks[1]).catch(err => console.log(err))
-      await client.publish(screenTracks).catch(err => console.log(err))
+      await client.unpublish(tracks[1]).catch((err: any) => console.log(err));
+      await client.publish(screenTracks).catch((err: any) => console.log(err));
     } else {
-      await client.publish(screenTracks).catch(err => console.log(err))
+      await client.publish(screenTracks).catch((err: any) => console.log(err));
     }
-  }
+  };
   const stopShareScreen = async () => {
     if (hasCam) {
-      await client.unpublish(screenTracks).catch(err => console.log(err))
-      await client.publish(trackCam[1]).catch(err => console.log(err))
+      await client
+        .unpublish(screenTracks)
+        .catch((err: any) => console.log(err));
+      await client.publish(trackCam[1]).catch((err: any) => console.log(err));
     } else {
-      await client.unpublish(screenTracks).catch(err => console.log(err))
+      await client
+        .unpublish(screenTracks)
+        .catch((err: any) => console.log(err));
     }
+  };
 
-  }
-  
   useEffect(() => {
-    startShareScreen();       
-    },[screenTracks])
+    startShareScreen();
+  }, [screenTracks]);
 
   return (
-    <div onClick={() => {
-      setShareScreen(false);
-      stopShareScreen()
-    }}>Tắt share</div>
-  )
+    <div
+      onClick={() => {
+        setShareScreen(false);
+        stopShareScreen();
+      }}>
+      Tắt share
+    </div>
+  );
 }
